8152e6ce831d9286a33ad5833a8a27cbe3ff2340
Katie Buchholz <kzbindy@hotmail.com>
2024-08-05 16:59:01 -0400

    started working on the typeDefs and edited the resolvers file

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -8,11 +8,53 @@ const resolvers = {
         user: async (parent, { _id }) => {
             const params = _id ? { _id } : {};
             return User.find(params);
+        },
     },
-},
 
     Mutation: {
         // create new user with token
+        addUser: async (parent, { username, email, password }) => {
+            const user = await User.create({ username, email, password });
+            const token = signToken(user);
+            return { token, user }
+        },
+        login: async (parent, { email, password }) => {
+            const user = await User.findOne({ email });
+            if (!user) {
+                throw AuthenticationError;
+            }
+
+            const correctPw = await user.isCorrectPassword(password);
+
+            if (!correctPw) {
+                throw AuthenticationError;
+            }
+
+            const token = signToken(user);
+
+            return { token, user };
+        },
+        saveBook: async (parent, { user, params }, context) => {
+            if (context.user) {
+                const book = await Book.findOneAndUpdate(
+                    { _id: user._id },
+                    { $addToSet: { savedBooks: body } },
+                    { new: true, runValidators: true }
+                );
+                return book;
+            }
+            throw AuthenticationError;
+        },
+        deleteBook: async (parent, { user, params }, context) => {
+            if (context.user) {
+                const book = await Book.findOneAndUpdate(
+                    { _id: user._id },
+                    { $pull: { savedBooks: { bookId: params.bookId } } },
+                    { new: true }
+                );
+                throw AuthenticationError;
+            }
+        }
     },
 };
 
---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,30 @@
+const typeDefs = `
+type User {
+    _id: ID
+    username: String!
+    email: String!
+    password: String!
+    savedBooks: [Book]
+}
+type Book {
+bookId: String!
+authors: [String]
+description: String!
+title: String!
+image: String
+link: String
+}
+
+type Auth {
+token: ID!
+user: User
+}
+
+type Query {
+users: 
+}
+
+type Mutation {
+
+}
+`;
